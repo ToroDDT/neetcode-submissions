91ca8bf49f834ba7f51b792efcc329a721cd41ec
David Del Toro <142459893+ToroDDT@users.noreply.github.com>
2026-06-10 18:37:46 -0700

    Add: is-anagram - submission-13

---
+++ Data Structures & Algorithms/is-anagram/submission-13.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] wordOne = s.toCharArray();
+        char[] wordTwo = t.toCharArray();
+        Arrays.sort(wordOne);
+        Arrays.sort(wordTwo);
+        return Arrays.equals(wordOne, wordTwo);
+    }
+}
