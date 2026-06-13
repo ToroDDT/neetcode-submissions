8bf19974c18567f782e5509605725bb513990b35
David Del Toro <142459893+ToroDDT@users.noreply.github.com>
2026-06-13 15:19:19 -0700

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        if (nums == null){
+            return false;
+        }
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            else {
+                set.add(num);
+            }
+        }
+        return false;
+    }
+}
