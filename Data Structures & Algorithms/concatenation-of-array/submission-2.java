f94dc2b3ee53dadb6cb26e924c6657832d747915
David Del Toro <142459893+ToroDDT@users.noreply.github.com>
2026-07-05 15:37:24 -0700

    Add: concatenation-of-array - submission-2

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-2.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        return java.util.stream.IntStream.concat(
+    java.util.Arrays.stream(nums), 
+    java.util.Arrays.stream(nums)
+).toArray();
+    }
+}
