464748c5bd034cb03f1cacff0aa524ba5b7b24d0
David Del Toro <142459893+ToroDDT@users.noreply.github.com>
2026-08-05 19:16:13 -0700

    Add: subsets - submission-11

---
+++ Data Structures & Algorithms/subsets/submission-11.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public List<List<Integer>> res;
+
+    public List<List<Integer>> subsets(int[] nums) {
+        res = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+        dfs(0, nums, subset);
+        return res;
+    }
+    public void dfs(int i, int[] nums, List<Integer> subset) {
+        if (i >= nums.length) {
+            res.add(new ArrayList<>(subset));
+            return;
+        }
+        // include this number and finds all subsets that include this number
+        subset.add(nums[i]);
+        dfs(i + 1, nums, subset);
+
+        //exclude this number and finds all subsets that exclude this number
+        subset.remove(subset.size() - 1);
+        dfs(i + 1, nums, subset);
+    }
+
+}
