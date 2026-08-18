816dc5b4f2fa8556a893bbbef19f668ce009e070
David Del Toro <142459893+ToroDDT@users.noreply.github.com>
2026-08-17 18:39:25 -0700

    Add: subsets - submission-23

---
+++ Data Structures & Algorithms/subsets/submission-23.java
@@ -0,0 +1,19 @@
+class Solution {
+    List<List<Integer>> res;
+    public List<List<Integer>> subsets(int[] nums) {
+        res = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+        dfs(nums, 0, subset);
+        return res;
+    }
+    public void dfs(int[] nums, int i, List<Integer> subset) {
+        if (i >= nums.length) {
+            this.res.add(new ArrayList<>(subset));
+            return;
+        }
+        subset.add(nums[i]);
+        dfs(nums, i + 1, subset);
+        subset.remove(subset.size() - 1);
+        dfs(nums, i + 1, subset);
+    }
+}
