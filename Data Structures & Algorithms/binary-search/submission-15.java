65f4f4b7fc6f2ef538c763344d801a0fc69e674d
David Del Toro <142459893+ToroDDT@users.noreply.github.com>
2026-08-31 12:38:31 -0700

    Add: binary-search - submission-15

---
+++ Data Structures & Algorithms/binary-search/submission-15.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        // the low
+        int l = 0;
+        // the high
+        int r = nums.length - 1;
+        while (l <= r) {
+            int mid = (r + l) >>> 1;
+            if (target == nums[mid]) {
+                return mid;
+            }
+            else if (nums[mid] > target) {
+                r = mid - 1;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
