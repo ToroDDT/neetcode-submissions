e910cbf48b80ebc1fdc3dd5f6935117a5bd29a33
David Del Toro <142459893+ToroDDT@users.noreply.github.com>
2026-09-13 11:59:40 -0700

    Add: min-cost-climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-5.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] memo = new int[n];
+        Arrays.fill(memo, -1);
+        return Math.min(dfs(0, cost, memo), dfs(1, cost, memo));
+    }
+    public int dfs(int i, int[] cost, int[] memo) {
+        if (i >= cost.length) return 0;
+        if (memo[i] != -1) return memo[i];
+        memo[i] = cost[i] + Math.min(dfs(i + 1, cost, memo), dfs(i + 2, cost, memo));
+        return memo[i];
+    }
+}
