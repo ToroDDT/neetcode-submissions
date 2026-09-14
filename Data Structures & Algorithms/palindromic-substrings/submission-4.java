1903c347f306021da7bb0cc2d2a22fead34fd96b
David Del Toro <142459893+ToroDDT@users.noreply.github.com>
2026-09-13 20:17:08 -0700

    Add: palindromic-substrings - submission-4

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-4.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j) && (j - i <= 2 || dp[i + 1][j - 1])){
+                    dp[i][j] = true;
+                }
+            }
+        }
+        int res = 0;
+        for (boolean[] list : dp){
+            for (boolean value : list) {
+                if (value == true) res++;
+            }
+        }
+        return res;
+    }
+}
