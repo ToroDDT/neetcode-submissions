4111d3f7300af91c24c67b3265958374ee1aa400
David Del Toro <142459893+ToroDDT@users.noreply.github.com>
2026-09-17 16:39:01 -0700

    Add: climbing-stairs - submission-18

---
+++ Data Structures & Algorithms/climbing-stairs/submission-18.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] memo = new int[n];
+        Arrays.fill(memo, -1);
+        return dfs(0, n, memo);
+    }
+
+    public int dfs(int i, int n, int[] memo) {
+        // check if i is in a valid positons, if at the end the only valid amojnt of stairs left to hit the top is one 
+        if (i >= n) return i == n ? 1 : 0;
+        if (memo[i] != -1) return memo[i];
+        return memo[i] = dfs(i + 1, n, memo) + dfs(i + 2, n, memo);
+    }
+}
+
+
+// what is the state 
+// the state is the position, the variable that can track the positoin 
+// can be (i);
+// so what is the dp()?
+// dp(i) = from i what are all the possible ways to reach the top
+// what choices i can do?
+// i can choose one step or two steps from position i
+// if computed (i) already, the amount of possible choices remain the same always, so i can use memoization 
