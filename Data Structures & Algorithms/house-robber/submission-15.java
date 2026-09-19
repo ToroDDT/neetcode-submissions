ac15731066274aedc6c20ea4c092e0086cf4a7e1
David Del Toro <142459893+ToroDDT@users.noreply.github.com>
2026-09-18 18:06:34 -0700

    Add: house-robber - submission-15

---
+++ Data Structures & Algorithms/house-robber/submission-15.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int rob(int[] nums) {
+        int prev1 = 0;
+        int prev2 = 0;
+        for (int num : nums) {
+            int current = Math.max(prev1, num + prev2);
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1;
+    }
+}
