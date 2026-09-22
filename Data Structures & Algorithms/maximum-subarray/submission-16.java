3af86bedabe1f5950dfaa9acb9f5f0fe5869e9f6
David Del Toro <142459893+ToroDDT@users.noreply.github.com>
2026-09-21 19:15:13 -0700

    Add: maximum-subarray - submission-16

---
+++ Data Structures & Algorithms/maximum-subarray/submission-16.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int curMax = 0; int globalMax = nums[0];
+        for(int num : nums) {
+            if (curMax < 0) {
+                curMax = 0;
+            }
+            curMax += num;
+            globalMax= Math.max(globalMax, curMax);
+        }
+        return globalMax;
+    }
+}
